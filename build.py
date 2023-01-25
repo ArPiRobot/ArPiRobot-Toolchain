e459cb91dc49c3bec6c1cf5206e79f1e2eb3d5ee
Marcus Behel <marcus.behel@mindspring.com>
2023-01-25 19:08:37 +0000

    Implemented full build

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 import sys
 import platform
+import shutil
 
 
 def input_int(prompt: str, lower: int, upper: int) -> int:
@@ -45,7 +46,7 @@ def input_yn(prompt: str, default: str) -> bool:
 
 if __name__ == "__main__":
     try:
-        script_dir = os.path.dirname(__file__)
+        script_dir = os.path.abspath(os.path.dirname(__file__))
         target_dir = os.path.join(script_dir, "targets")
         host_dir = os.path.join(script_dir, "hosts")
     
@@ -107,12 +108,16 @@ if __name__ == "__main__":
             cf.write(os.linesep)
             with open(os.path.join(host_dir, sel_host)) as hf:
                 cf.write(hf.read())
+            cf.write(os.linesep)
+            cf.write("# Extra config")
+            cf.write(os.linesep)
+            cf.write("# CT_PREFIX_DIR_RO is not set")
+            cf.write(os.linesep)
 
         print("================================================================================")
         print("ct-ng menuconfig")
         print("================================================================================")
         print("Press enter to run ct-ng menuconfig.")
-        print("It is recommended to disable 'render readonly' under 'paths and misc options'.")
         print("Then, save as '.config' and exit.")
         input()
         try:
@@ -139,6 +144,73 @@ if __name__ == "__main__":
             print("ct-ng not found!")
             exit(1)
         print("================================================================================")
+
+        print("================================================================================")
+        print("Generate toolchain zip")
+        print("================================================================================")
+        # Determine the host tuple (determines toolchain path in ~/x-tools/)
+        host_tuple = ""
+        with open(os.path.join(script_dir, ".config")) as f:
+            while True:
+                line = f.readline()
+                if line == "" or line is None:
+                    break
+                if line.startswith("CT_HOST="):
+                    # CT_HOST="tupple-we-want-is-here"
+                    host_tuple = line.strip()[9:-1]
+        
+        # Get toolchain target tuple
+        target_tuple = ""
+        try:
+            p = subprocess.Popen(["ct-ng", "show-tuple"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if p.wait() != 0:
+                print("ct-ng show-tuple exited with non-zero return code.")
+                exit(1)
+            target_tuple = p.stdout.readline().strip().decode()
+        except FileNotFoundError:
+            print("ct-ng not found!")
+            exit(1)
+
+        # Construct toolchain directory path
+        toolchain_dir = ""
+        if host_tuple == "":
+            toolchain_dir = "{}/x-tools/{}/".format(os.environ["HOME"], target_tuple)
+        else:
+            toolchain_dir = "{}/x-tools/HOST-{}/{}/".format(os.environ["HOME"], host_tuple, target_tuple)
+
+        # Add what.txt file
+        target_name = sel_target[:-7]
+        with open(os.path.join(toolchain_dir, "what.txt"), 'w') as f:
+            f.write("toolchain/{}".format(target_name))
+        
+        # Add version.txt
+        ver = input("Version string: ")
+        # Add what.txt file
+        with open(os.path.join(toolchain_dir, "version.txt"), 'w') as f:
+            f.write(ver)
+
+        # Remove build log (large file)
+        if os.path.exists(os.path.join(toolchain_dir, target_tuple, "build.log.bz2")):
+            os.remove(os.path.join(toolchain_dir, target_tuple, "build.log.bz2"))
+
+        # Make zip file
+        host_name = sel_host[:-7]
+        old_wd = os.curdir
+        os.chdir(toolchain_dir)
+        try:
+            zip_name = "ArPiRobot-Toolchain-{}-{}.zip".format(target_name, host_name)
+            print(" ".join(["zip", os.path.join(script_dir, zip_name), "-r", "."]))
+            p = subprocess.Popen(["zip", os.path.join(script_dir, zip_name), "-r", "."], stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr, bufsize=0)
+            if p.wait() != 0:
+                print("zip exited with non-zero return code.")
+                exit(1)
+        except FileNotFoundError:
+            print("zip not found!")
+            exit(1)
+        os.chdir(old_wd)
+        print("================================================================================")
+
+        exit(0)
     except KeyboardInterrupt:
         exit(1)
 
