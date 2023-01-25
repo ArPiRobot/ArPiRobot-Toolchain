e386d804993a11de09f8b6983a0cebd5430be8f6
Marcus Behel <marcus.behel@mindspring.com>
2023-01-25 12:28:18 -0500

    Added build commands to python script

--- build.py
+++ build.py
@@ -94,16 +94,46 @@ if __name__ == "__main__":
         print("================================================================================")
         print()
 
+        if os.path.exists(os.path.join(script_dir, ".config")):
+            os.remove(os.path.join(script_dir, ".config"))
+        with open(os.path.join(script_dir, ".config"), 'w') as cf:
+            cf.write("# Target configuration")
+            cf.write(os.linesep)
+            with open(os.path.join(target_dir, sel_target)) as tf:
+                cf.write(tf.read())
+            cf.write(os.linesep)
+            cf.write(os.linesep)
+            cf.write("# Host configuration")
+            cf.write(os.linesep)
+            with open(os.path.join(host_dir, sel_host)) as hf:
+                cf.write(hf.read())
+
         print("================================================================================")
         print("ct-ng menuconfig")
         print("================================================================================")
         print("Press enter to run ct-ng menuconfig.")
-        print("Save as '.config' and exit.")
+        print("It is recommended to disable 'render readonly' under 'paths and misc options'.")
+        print("Then, save as '.config' and exit.")
         input()
         try:
             p = subprocess.Popen(["ct-ng", "menuconfig"], stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr, bufsize=0)
             if p.wait() != 0:
                 print("ct-ng menuconfig exited with non-zero return code.")
+                exit(1)
+        except FileNotFoundError:
+            print("ct-ng not found!")
+            exit(1)
+        print("================================================================================")
+        print()
+
+        print("================================================================================")
+        print("ct-ng build")
+        print("================================================================================")
+        ncores = input_int("Build threads: ", 1, 999999)
+        try:
+            p = subprocess.Popen(["ct-ng", "build.{}".format(ncores)], stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr, bufsize=0)
+            if p.wait() != 0:
+                print("ct-ng build exited with non-zero return code.")
                 exit(1)
         except FileNotFoundError:
             print("ct-ng not found!")
